084257856c81cafc986598fc214594f343172961
Thomas Lam <thomaslam2142@gmail.com>
2023-11-07 20:31:06 -0500

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Im a web developer from Toronto, Canada.
+    </p>
+    <div>
+      <div>
+        <a href="https://github.com/ThomasLam2142">Github</a>
+      </div>
+      
+      <div>
+        <a href="https://www.linkedin.com/in/thomas-lam-872740117/">Linkedin</a>
+      </div>
+  </div>
+  </div>
+  
+</template>
+
+<style>
+
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Contact Us</h1>
+        <form>
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+
+            <button type="submit">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactVue'
+}
+</script>
+
+<style>
+/* Add custom styles here */
+</style>
