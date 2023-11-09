17364e073135f89d1e379b988de2969e613639a9
Thomas Lam <thomaslam2142@gmail.com>
2023-11-08 20:19:02 -0500

    a bit of styling

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -66,7 +66,7 @@ function clear(){
 <template>
   <main>
     <h1>Calculator</h1>
-    <div>
+    <div id="mainDisplay">
         {{ count }}
     </div>
     <div>
@@ -162,5 +162,15 @@ button{
 #equalsBtn{
     width: 100%;
     height: 100%;
+}
+
+#mainDisplay{
+    width: 100%;
+    height: 100px;
+    border: 1px solid black;
+    text-align: right;
+    padding-right: 10px;
+    font-size: 50px;
+    margin-bottom: 10px;
 }
 </style>
